c1005e7e7be90c5e9648894cffc3a426ea38d506
Lydia Charles <lydiatpcharles@gmail.com>
2023-05-14 22:47:57 -0700

    Animatable

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,18 +1,21 @@
 import { ScrollView } from "react-native-gesture-handler";
 import { Card, Text } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
 
 const ContactScreen = () => {
   return (
     <ScrollView>
-      <Card wrapperStyle={{ margin: 10 }}>
-        <Card.Title>Contact Information</Card.Title>
-        <Text>1 Nucamp Way</Text>
-        <Text>Seattle, WA 98001</Text>
-        <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
-        <Text>Phone: 1-206-555-1234</Text>
-        <Text>Email: campsites@nucamp.co</Text>
-        <Card.Divider />
-      </Card>
+      <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
+        <Card wrapperStyle={{ margin: 10 }}>
+          <Card.Title>Contact Information</Card.Title>
+          <Text>1 Nucamp Way</Text>
+          <Text>Seattle, WA 98001</Text>
+          <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+          <Text>Phone: 1-206-555-1234</Text>
+          <Text>Email: campsites@nucamp.co</Text>
+          <Card.Divider />
+        </Card>
+      </Animatable.View>
     </ScrollView>
   );
 };
